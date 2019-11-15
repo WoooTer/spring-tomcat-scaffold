94aa3dbdbad09eafdf52625baca14c445a73a90b
WooTer <wutao1155@qq.com>
2019-11-15 16:10:47 +0800

    add es6 module eg.

---
+++ src/main/webapp/js/es-basic/es6/module/main.js
@@ -0,0 +1,13 @@
+// main.js
+import { firstName, lastName, year, multiply } from './profile.js';
+
+function setName() {
+    console.log(firstName + ' ' + lastName + ' in ' + year);
+}
+
+setName();
+
+var result = multiply(2, 9);
+console.log(result);
+
+console.log(myValue);
---
+++ src/main/webapp/js/es-basic/es6/module/profile.js
@@ -0,0 +1,11 @@
+var firstName = 'Michael';
+var lastName = 'Jackson';
+var year = 1958;
+
+export { firstName, lastName, year };
+
+export function multiply(x, y) {
+    return x * y;
+};
+
+window.myValue = "oh my god!";
